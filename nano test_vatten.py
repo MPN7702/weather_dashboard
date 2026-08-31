4ebe06e39a67d3aad7344b3a1efa3b79cb628a35
MPN7702 <mr_magoo21@hotmail.com>
2026-08-31 18:04:49 +0200

    Update nano test_vatten.py

--- nano test_vatten.py
+++ nano test_vatten.py
@@ -1,1 +1,13 @@
+import requests
 
+url = "https://sv.seatemperature.net/sjoar/water-temp-in-mellanfryken"
+
+html = requests.get(
+    url,
+    headers={
+        "User-Agent": "Mozilla/5.0"
+    },
+    timeout=10
+).text
+
+print(html[:5000])
