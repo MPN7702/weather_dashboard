34355fff8d310bb3046785e2b4cf76393721f6bb
MPN7702 <mr_magoo21@hotmail.com>
2026-08-31 19:33:27 +0200

    Update test_vatten.py

--- test_vatten.py
+++ test_vatten.py
@@ -1,14 +1,13 @@
 import requests
 
-url = "https://sv.seatemperature.net/sjoar/water-temp-in-mellanfryken"
+url = "https://fangstrapport.se/sjö/övre-fryken/vattentemperatur"
 
-html = requests.get(
+response = requests.get(
     url,
     headers={"User-Agent": "Mozilla/5.0"},
-    timeout=10
-).text
+    timeout=20
+)
 
-print("temp-value =", html.find("temp-value"))
-print("Aktuell vattentemperatur =", html.find("Aktuell vattentemperatur"))
-print("Vattentemperatur i sjön =", html.find("Vattentemperatur i sjön"))
-print("18.5 =", html.find("18.5"))
+print("Status:", response.status_code)
+print(response.url)
+print(response.text[:3000])
